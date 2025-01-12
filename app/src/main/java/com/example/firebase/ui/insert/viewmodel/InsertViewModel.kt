8eaad8dfda50e05dd00b11dabbdd7da0ed203479
IAMHUDA <hudamiftahul362@gmail.com>
2025-01-12 15:03:00 +0700

    add insertview model

---
+++ app/src/main/java/com/example/firebase/ui/insert/viewmodel/InsertViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.firebase.ui.insert.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.firebase.repository.RepositoryMhs
+
+class InsertViewModel(
+    private val repositoryMhs: RepositoryMhs
+): ViewModel() {}
