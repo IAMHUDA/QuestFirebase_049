0ae3184c3dad77fc4935b1ba0f0cc31e78de37e5
IAMHUDA <hudamiftahul362@gmail.com>
2025-01-12 14:59:37 +0700

    home view add ondeleteclick

--- app/src/main/java/com/example/firebase/ui/home/pages/HomeView.kt
+++ app/src/main/java/com/example/firebase/ui/home/pages/HomeView.kt
@@ -87,7 +87,7 @@ fun HomeScreen(
             modifier = Modifier.padding(innerPadding),
             onDetailClick = onDetailClick,
             onDeleteClick = {
-                viewModel.getMhs()
+                viewModel.deleteMhs(it)
             }
         )
     }
