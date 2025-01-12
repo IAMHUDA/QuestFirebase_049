765ad53c2ba0a4b6bf504eb7ca2c2e8685a5e493
IAMHUDA <hudamiftahul362@gmail.com>
2025-01-12 15:15:19 +0700

    penyedia viewmodel add insertviewmodel

--- app/src/main/java/com/example/firebase/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/PenyediaViewModel.kt
@@ -6,11 +6,18 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.firebase.MahasiswaApplications
 import com.example.firebase.ui.home.viewmodel.HomeViewModel
+import com.example.firebase.ui.insert.viewmodel.InsertViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             HomeViewModel(
+                mahasiswaApp().container.repositoryMhs
+            )
+        }
+
+        initializer {
+            InsertViewModel(
                 mahasiswaApp().container.repositoryMhs
             )
         }
