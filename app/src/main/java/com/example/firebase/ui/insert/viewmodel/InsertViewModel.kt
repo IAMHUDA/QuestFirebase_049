941404dd75536d6e4f3c50ec35d3b9485b047715
IAMHUDA <hudamiftahul362@gmail.com>
2025-01-12 15:06:48 +0700

    add fun insertMahasiswa

--- app/src/main/java/com/example/firebase/ui/insert/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/insert/viewmodel/InsertViewModel.kt
@@ -4,7 +4,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.firebase.repository.RepositoryMhs
+import kotlinx.coroutines.launch
 
 class InsertViewModel(
     private val repositoryMhs: RepositoryMhs
@@ -34,5 +36,20 @@ class InsertViewModel(
 
         uiEvent = uiEvent.copy(isEntryValid = errorState)
         return errorState.isValid()
+    }
+    fun insertMhs(){
+        if (validateFields()) {
+            viewModelScope.launch {
+                uiState = FormState.Loading
+                try {
+                    repositoryMhs.insertMhs(uiEvent.insertUiEvent.toMhsModel())
+                    uiState = FormState.Success("Data Berhasil Disimpan")
+                } catch (e: Exception) {
+                    uiState = FormState.Error("Data Gagal Disimpan")
+                }
+            }
+        } else {
+            uiState = FormState.Error("Data Tidak Valid")
+        }
     }
 }
