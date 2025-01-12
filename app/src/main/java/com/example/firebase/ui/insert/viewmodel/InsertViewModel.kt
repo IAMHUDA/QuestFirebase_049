e5f9605121d93fc59928a3a25cab0ed8efc3bb49
IAMHUDA <hudamiftahul362@gmail.com>
2025-01-12 15:04:37 +0700

    add insertviewmodel make var insertuievent

--- app/src/main/java/com/example/firebase/ui/insert/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/insert/viewmodel/InsertViewModel.kt
@@ -1,8 +1,14 @@
 package com.example.firebase.ui.insert.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.firebase.repository.RepositoryMhs
 
 class InsertViewModel(
     private val repositoryMhs: RepositoryMhs
-): ViewModel() {}
+): ViewModel() {
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+}
