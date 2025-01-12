28c4d723e78637900427144f0b17336b42704357
IAMHUDA <hudamiftahul362@gmail.com>
2025-01-12 15:49:11 +0700

    add val uistate,uievent,snackbar,coroutine

--- app/src/main/java/com/example/firebase/ui/insert/pages/InsertView.kt
+++ app/src/main/java/com/example/firebase/ui/insert/pages/InsertView.kt
@@ -1,6 +1,9 @@
 package com.example.firebase.ui.insert.pages
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.firebase.ui.PenyediaViewModel
@@ -13,5 +16,10 @@ fun InsertMhsView(
     modifier: Modifier = Modifier,
     viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
+    val uiState = viewModel.uiState
+    val uiEvent = viewModel.uiEvent
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
 
 }
